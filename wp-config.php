cea9eea89a3cb4b8089d656c8ceb361c29a58358
Zack Tollman <tollmanz@gmail.com>
2015-01-04 09:50:40 -0800

    Adding config for memcached object cache.

--- wp-config.php
+++ wp-config.php
@@ -4,6 +4,13 @@ if ( file_exists( dirname(__FILE__) . '/../zdt-me-config.php' ) ) {
 	require_once( dirname(__FILE__) . '/../zdt-me-config.php' );
 }
 
+global $memcached_servers;
+$memcached_servers = array(
+	'default' => array(
+		'127.0.0.1:11211'
+	),
+);
+
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
 	define('ABSPATH', dirname(__FILE__) . '/');
