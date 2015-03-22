ea71bfde9c69dccc89faf21b49192e837443a0c0
Zack Tollman <tollmanz@gmail.com>
2015-03-22 12:21:01 -0700

    Add another google domain to prefetch.

--- wp-content/mu-plugins/dns-prefetch.php
+++ wp-content/mu-plugins/dns-prefetch.php
@@ -4,6 +4,7 @@ function zdt_dns_prefetch() {
 ?>
 	<link rel="dns-prefetch" href="//fonts.googleapis.com">
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link rel="dns-prefetch" href="//ajax.googleapis.com">
 <?php
 }
 
