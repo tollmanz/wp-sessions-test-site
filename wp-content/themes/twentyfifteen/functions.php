98e3f8a4d643880d729047415120f1ca20674adc
Zack Tollman <tollmanz@gmail.com>
2015-03-22 10:24:43 -0700

    Version bump the genericons.

--- wp-content/themes/twentyfifteen/functions.php
+++ wp-content/themes/twentyfifteen/functions.php
@@ -203,7 +203,7 @@ function twentyfifteen_scripts() {
 	wp_enqueue_style( 'twentyfifteen-fonts', twentyfifteen_fonts_url(), array(), null );
 
 	// Add Genericons, used in the main stylesheet.
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.2' );
+	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/genericons/genericons.css', array(), '3.3' );
 
 	// Load our main stylesheet.
 	wp_enqueue_style( 'twentyfifteen-style', get_stylesheet_uri() );
