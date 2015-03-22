bca5df481bf1b9bbac1a1702650a7ca921523fed
Zack Tollman <tollmanz@gmail.com>
2015-03-22 12:01:57 -0700

    Async load Google fonts.

--- wp-content/mu-plugins/async.php
+++ wp-content/mu-plugins/async.php
@@ -1,11 +1,35 @@
 <?php
 
-function zdt_async( $tag, $handle ) {
-	if ( 'twentyfifteen-fonts-css' !== $handle ) {
-		return $tag;
-	}
-
-	return str_replace( ' src', ' defer="defer" src', $tag );
+function zdt_remove_google_fonts() {
+	wp_deregister_style( 'twentyfifteen-fonts' );
 }
 
-add_filter( 'script_loader_tag', 'zdt-async', 10, 2 );
+add_filter( 'wp_enqueue_scripts', 'zdt_remove_google_fonts', 11 );
+
+function zdt_async_load_fonts() {
+?>
+	<script type="text/javascript">
+		WebFontConfig = {
+			google: {
+				families: [
+					'Noto Sans:400italic,700italic,400,700',
+					'Noto Serif:400italic,700italic,400,700',
+					'Inconsolata:400,700'
+				]
+			}
+		};
+
+		(function() {
+			var wf = document.createElement('script');
+			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+			'://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+			wf.type = 'text/javascript';
+			wf.async = 'true';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wf, s);
+		})();
+	</script>
+<?php
+}
+
+add_action( 'wp_footer', 'zdt_async_load_fonts' );
